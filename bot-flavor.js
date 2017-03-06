bb0eabb7c80f4c32f60f3b4a42d1044800ff50d5
Abi Klein <abi920@gmail.com>
2017-03-06 14:56:47 -0500

    attempts to set up basic bot without customization

---
+++ bot-flavor.js
@@ -0,0 +1,36 @@
+var bot_flavor = {
+  "present": "Hello, you're here.",
+  "already_queued": null,
+  "secret_set": null,
+  "remove": "Goodbye! :wave:",
+  "empty_queue": null,
+  "attendance_cleared": "Attendance cleared",
+  "attendance_zero": null,
+  "greeting": `
+              Hello. I dont believe
+              we have been introduced.
+              R2-D2? A pleasure
+              to meet you. I am C-3PO,
+              Human-Cyborg Relations.
+              `,
+  "quotes": [
+    "BWAAAK Did you read the docs?",
+    "SQWUAK check the docs!",
+    "BRAAAAK Did you ask your buddy?",
+    "What is the most favorite pirating song, surprisingly? Don’t cry for me, Arrrrrrgentina...",
+    "Have you ever heard any good pirate jokes? Well neither have ayyye!",
+    "How do pirates communicate with each other? With an Aye phone.",
+    "What is regularly given to the sea around 8 am, if the digestion is right? The captain’s log.",
+    'Which side of his boat does a good pirate try to avoid? The outside.',
+    "What would you call a pirate with 4 eyes?  An iiiirate.",
+    "Why are pirates bad at cards? It’s kind of hard to play when you’re sitting on the deck.",
+    "What does the pirate say when he steps on a Lego? Arrrrrgh!!!",
+    "To err is human. To arr is seriously pirate.",
+    "How does a pirate declutter his ship? By having a yarrrrd sale.",
+    "Wooden pegs and hooks are really expensive these days. They cost an arm and a leg.",
+    "Why are pirates good programmers? Because they spend months and months at C.",
+  "Have you heard about the new pirate movie? It's rated AARRRRRR!"
+],
+};
+
+module.exports = bot_flavor;
